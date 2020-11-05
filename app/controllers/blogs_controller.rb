b34440f8dc7bbaaed2ccc8a5a1d6ce5be1b70954
ymymz87@icloud.com <ec2-user@ip-172-31-78-109.ec2.internal>
2020-11-05 06:18:35 +0000

    [finish]3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,7 +15,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blog_path(blogid)
+    redirect_to blog_path(blog.id)
   end
 
   def edit
